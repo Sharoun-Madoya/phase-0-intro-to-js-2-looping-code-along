7816163d0bcdcbdc4cf0003dc575bbc645a4b0ae
[sharoun <[madoyasharoun@gmail.com]>
2023-03-12 22:26:48 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solutions in this file
+let countdown = 10; 
+    while (countdown < 0;) ) {
+    console.log(countdown--);
+}
+let countdown = 0;
+while (countdown < 10) {
+  console.log(countdown--);
+}
