47273d1b47c04482dd10a041bb7cdc7c402eedb7
[sharoun <[madoyasharoun@gmail.com]>
2023-03-20 19:31:57 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,9 +1,46 @@
 // Code your solutions in this file
-let countdown = 10; 
-    while (countdown < 0;) ) {
-    console.log(countdown--);
+// for (let age = 30; age < 40; age++) {
+//   console.log(`I'm ${age} years old. Happy birthday to me!`);
+  
+// }
+
+// const gifts = ["teddy bear", "drone", "doll"];
+
+// function wrapGifts(gifts) {
+//   for (let i = 0; i < gifts.length; i++) {
+//     console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    
+//   }
+
+//   return gifts;
+// }
+
+// wrapGifts(gifts);
+
+// function writeCards(name, event) {
+//   const messages =[];
+//   for (let i = 0; i < name.length; i++) {
+//     messages.push(`Thank you, ${name [i]}, for the wonerful ${event} gift!`)
+//   }
+//   return messages;
+// }
+
+
+function writeCards(names, event){
+  const messages = [];
+  for (let i = 0;
+      i < names.length;
+      i++) {
+  messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+      }
+      return messages;
 }
-let countdown = 0;
-while (countdown < 10) {
-  console.log(countdown--);
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise")
+let number = 10;
+function countDown(number) {
+    while (number > 0) {
+        console.log(number);
+        number-=1;
+    }
+    console.log(number);
 }
